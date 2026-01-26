159551484e0d6e6172e33d50f637e3f9fbde0ace
Bobla-2 <savva050203@gmail.com>
2026-01-26 15:59:22 +0300

    .

--- source/main.py
+++ source/main.py
@@ -47,6 +47,9 @@ for i, path in enumerate(paths):
     total_code_file += '\n'
 
 doc = Document(sample_file)
+# for style in doc.styles:
+# #     if style.type == 1 and not style.builtin:  # paragraph styles, кастомные
+#       print(style.name)
 for p in doc.paragraphs:
     if '<КОД ПРОГРАММЫ>' in p.text:
         p.text = ''
@@ -57,17 +60,18 @@ for p in doc.paragraphs:
                 p.insert_paragraph_before(line.rstrip(), 'Heading 3')
             else:
                 p.insert_paragraph_before(line.rstrip(), 'КОД')
-    elif "<НАЗВАНИЕ>" in p.text:
+    elif "<НАЗВАНИЕ>" in p.text.upper():
         p.text = ''
-        p.insert_paragraph_before(prodl_name, "Title")
-    elif "<номер>" in p.text:
+        p.insert_paragraph_before(prodl_name, "Заг искл огл")
+    elif "<НОМЕР>" in p.text.upper():
         p.text = ''
-        p.insert_paragraph_before(filename[3:-18], "Title")
-    elif "<номе2р>" in p.text:
+        p.insert_paragraph_before(filename[3:-18], "Заг искл огл")
+    elif "<НОМЕР2>" in p.text.upper():
         p.text = ''
-        p.insert_paragraph_before(filename[3:-18] + "-ЛУ", "Title")
+        p.insert_paragraph_before(filename[3:-18] + "-ЛУ", "Заг искл огл")
 
-    elif "<номе1р>" in p.text:
+
+    elif "<НОМЕР1>" in p.text.upper():
         p.text = ''
         p.insert_paragraph_before("             " + filename[3:-18] + "-ЛУ", "еспд-дец-1")
 
@@ -78,4 +82,54 @@ for p in doc.paragraphs:
                 paragraph.text = ''
                 paragraph.insert_paragraph_before(filename[3:-18], "Title")
 
+from datetime import datetime
+year = str(datetime.now().year)
+
+
+def replace_year(container):
+    for p in container.paragraphs:
+        # print(p.text)
+        if "<data>" in p.text:
+            p.text = p.text.replace("<data>", year)
+
+    for table in container.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                replace_year(cell)
+
+def replace_num_dc(container):
+    for p in container.paragraphs:
+        # print(p.text)
+        if "<НОМЕР_ВХ>" in p.text.upper():
+            p.text = p.text.replace("<НОМЕР_ВХ>", filename[3:-18])
+
+    for table in container.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                replace_num_dc(cell)
+
+for section in doc.sections:
+    replace_year(section.footer)
+    replace_year(section.first_page_footer)
+    replace_num_dc(section.header)
+
+
+
+
 doc.save(filename + '.docx')
+
+
+
+import win32com.client as win32
+from pathlib import Path
+
+path = Path(filename + '.docx').resolve()
+
+word = win32.Dispatch("Word.Application")
+word.Visible = False
+
+doc = word.Documents.Open(str(path))
+doc.TablesOfContents(1).Update()   # обновить содержание
+doc.Save()
+doc.Close()
+word.Quit()
